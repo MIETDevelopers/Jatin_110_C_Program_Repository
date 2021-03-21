f1bb6ad1b7008eb6a7a2aeef2c968b1b32ba18e3
unknown <2020a1r110@mietjammu.in>
2021-03-21 02:43:47 -0700

    hello world

---
+++ hello_3.c
@@ -0,0 +1,7 @@
+//program to print hello world   
+#include <stdio.h> //pre processor directive to include input output function
+int main() //main function 
+{
+	printf("Hello World!"); //printing hello world
+	return 0;
+}
